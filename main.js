a0379a9aeaa759a00a0698b8649b9b1d0e39e2be
pinki-maker <pinkywork17@gmail.com>
2020-10-21 11:46:00 +0000

    Lab 1

--- main.js
+++ main.js
@@ -58,15 +58,15 @@ function get(serviceNowTable, callback) {
   // that holds the HTTP request method, credentials, and the API's URL.
   // Some properties are hardcoded, like the method and part of the URI.
   // Some properties are read from global const options.
-  // Some properties are passed into function post() through parameters.
+  // Some properties are passed into function get() through parameters.
   const requestOptions = {
-    method: 'POST',
+    method: 'GET',
     auth: {
       user: options.username,
       pass: options.password,
     },
     baseUrl: options.url,
-    uri: `/api/now/table/${serviceNowTable}`,
+    uri: `/api/now/table/${serviceNowTable}?sysparm_limit=1`,
   };
 
   // Send Request to ServiceNow.
@@ -95,68 +95,11 @@ function get(serviceNowTable, callback) {
   });
 
 }
-
-
-
-function get(serviceNowTable, callback) {
-
-  // Initialize return arguments for callback
-  let callbackData = null;
-  let callbackError = null;
-
-  // Construct API call to send to ServiceNow.
-  // The request constructor has an options parameter
-  // that holds the HTTP request method, credentials, and the API's URL.
-  // Some properties are hardcoded, like the method and part of the URI.
-  // Some properties are read from global const options.
-  // Some properties are passed into function post() through parameters.
-  const requestOptions = {
-    method: 'POST',
-    auth: {
-      user: options.username,
-      pass: options.password,
-    },
-    baseUrl: options.url,
-    uri: `/api/now/table/${serviceNowTable}`,
-  };
-
-  // Send Request to ServiceNow.
-  // We are passing variable requestOptions for the first argument.
-  // We are passing an anonymous function, an error-first callback,
-  // for the second argument.
-  request(requestOptions, (error, response, body) => {
-    /**
-     * Process ServiceNow error, response and body.
-     * Check error and response code to make sure
-     * response is good.
-     */
-    if (error) {
-      console.error('Error present.');
-      callbackError = error;
-    } else if (!validResponseRegex.test(response.statusCode)) {
-      console.error('Bad response code.');
-      callbackError = response;
-    } else if (response.body.includes('Instance Hibernating page')) {
-      callbackError = 'Service Now instance is hibernating';
-      console.error(callbackError);
-    } else {
-      callbackData = response;
-    }
-    return callback(callbackData, callbackError);
-  });
-
-}
-
-
-
-
-
-
 
 
 // This test function calls your request and logs any errors.
 function main() {
-  // Call the ServiceNow POST API
+  // Call function get().
   // We are passing a static argument for parameter serviceNowTable.
   // We are passing an anonymous function argument, a data-first callback,
   // for parameter callback.
